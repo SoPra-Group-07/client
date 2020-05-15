b97e0427c8b3091a43767d7f6a5cf00b8252676b
lzumta <lynn.zumtaugwald@uzh.ch>
2020-05-15 13:25:15 +0200

    try to deploy

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -1,90 +1,101 @@
-import React from 'react';
-import  { BaseContainer, FormContainer, Container, Users, PlayerContainer} from '../../helpers/layout';
-import { api, handleError } from '../../helpers/api';
-import { Spinner } from '../../views/design/Spinner';
-import {CustomizedButton } from '../../views/design/Button';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import {
+  BaseContainer,
+  FormContainer,
+  Container,
+  Users,
+  PlayerContainer,
+} from "../../helpers/layout";
+import { api, handleError } from "../../helpers/api";
+import { Spinner } from "../../views/design/Spinner";
+import { CustomizedButton } from "../../views/design/Button";
+import { withRouter } from "react-router-dom";
 import LeaderboardEntity from "../../views/LeaderboardEntity";
 
-
 class Leaderboard extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            users: null,
-            count: 0
-        };
+  constructor() {
+    super();
+    this.state = {
+      users: null,
+      count: 0,
+    };
+  }
+
+  back() {
+    sessionStorage.setItem("isValid", "true");
+    this.props.history.push(`/overview`);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  async updateLeaderboard() {
+    const response = await api.get("/leaderboards");
+
+    await new Promise((resolve) => setTimeout(resolve, 1002));
+
+    this.setState({ users: response.data });
+  }
+
+  async componentDidMount() {
+    try {
+      sessionStorage.setItem("isValid", "false");
+      const pathName = this.props.location.pathname;
+      sessionStorage.setItem("pathName", pathName);
+
+      this.updateLeaderboard();
+      this.interval = setInterval(async () => {
+        this.updateLeaderboard();
+      }, 30000);
+    } catch (error) {
+      alert(
+        `Something went wrong while fetching the users: \n${handleError(error)}`
+      );
     }
+  }
 
-    back() {
-        sessionStorage.setItem("isValid", "true");
-        this.props.history.push(`/overview`);
-    }
+  render() {
+    return (
+      <BaseContainer>
+        <FormContainer>
+          <Container>
+            <h2>LEADERBOARD</h2>
 
-    componentWillUnmount(){
-        clearInterval(this.interval);
-    }
-
-    async updateLeaderboard(){
-        const response = await api.get('/leaderboards');
-
-        await new Promise(resolve => setTimeout(resolve, 1002));
-
-        this.setState({ users: response.data });
-    }
-
-    async componentDidMount() {
-        try {
-            sessionStorage.setItem("isValid", "false");
-            const pathName = this.props.location.pathname;
-            sessionStorage.setItem("pathName", pathName);
-
-            this.updateLeaderboard();
-            this.interval = setInterval(async() => {
-                this.updateLeaderboard();
-            },30000);
-        } catch (error) {
-            alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
-        }
-    }
-
-    render() {
-        return (
-
-            <BaseContainer>
-                <FormContainer>
-                    <Container>
-                        <h2>LEADERBOARD</h2>
-                        
-                        {!this.state.users ? (
-                            <Spinner />
-                        ) : (
-                            <div>
-                                <Users>
-                                    {this.state.users.map(user => {
-                                        this.state.count ++;
-                                        return (
-                                            <PlayerContainer>
-                                                <LeaderboardEntity count={this.state.count} user={user}/>
-                                            </PlayerContainer>
-                                        );
-                                    })}
-                                </Users>
-                                <CustomizedButton
-                                    color1={"red"} color2={"darkred"} width = {"60%"}
-                                    onClick={() => {
-                                        this.back();
-                                    }}
-                                >
-                                    Back
-                                </CustomizedButton>
-                            </div>
-                        )}
-                    </Container>
-                </FormContainer>
-            </BaseContainer>
-        );
-    }
+            {!this.state.users ? (
+              <Spinner />
+            ) : (
+              <div>
+                <Users>
+                  {this.state.users.map((user) => {
+                    this.state.count++;
+                    return (
+                      <PlayerContainer>
+                        <LeaderboardEntity
+                          count={this.state.count}
+                          user={user}
+                        />
+                      </PlayerContainer>
+                    );
+                  })}
+                </Users>
+                <CustomizedButton
+                  color1={"red"}
+                  color2={"darkred"}
+                  width={"60%"}
+                  onClick={() => {
+                    this.back();
+                  }}
+                >
+                  Back
+                </CustomizedButton>
+              </div>
+            )}
+          </Container>
+        </FormContainer>
+      </BaseContainer>
+    );
+  }
 }
 
 export default withRouter(Leaderboard);
