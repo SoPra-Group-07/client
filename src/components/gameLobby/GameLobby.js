3921b02f15a92f5d0d7dc1076ce4e7ad298957a3
lzumta <lynn.zumtaugwald@uzh.ch>
2020-05-22 17:05:15 +0200

    deploy

--- src/components/gameLobby/GameLobby.js
+++ src/components/gameLobby/GameLobby.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
-import  { BaseContainer, FormContainer, ButtonContainer, Container, Users} from '../../helpers/layout';
-import { api, handleError } from '../../helpers/api';
-import { Spinner } from '../../views/design/Spinner';
-import {CustomizedButton } from '../../views/design/Button';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import {
+  BaseContainer,
+  FormContainer,
+  ButtonContainer,
+  Container,
+  Users,
+} from "../../helpers/layout";
+import { api, handleError } from "../../helpers/api";
+import { Spinner } from "../../views/design/Spinner";
+import { CustomizedButton } from "../../views/design/Button";
+import { withRouter } from "react-router-dom";
 import GameLobbyEntity from "../../views/GameLobbyEntity";
-
 
 const PlayerContainer = styled.li`
   display: flex;
@@ -16,159 +21,168 @@ const PlayerContainer = styled.li`
 `;
 
 class GameLobby extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            lobby: null,
-            alreadyClicked: false
-        };
+  constructor() {
+    super();
+    this.state = {
+      lobby: null,
+      alreadyClicked: false,
+    };
+  }
+
+  async startGame(id) {
+    try {
+      sessionStorage.setItem("isValid", "true");
+      this.setState({ alreadyClicked: true });
+      const requestBody = JSON.stringify({
+        gameId: id,
+      });
+
+      const response = await api.put(`/games/lobby`, requestBody);
+      //console.log(response.data);
+
+      sessionStorage.setItem("GameRoundId", response.data.gameRoundId);
+      sessionStorage.setItem("points", 0.0);
+      sessionStorage.setItem("TotalGameRounds", "X");
+      this.props.history.push(`/games/${id}`);
+    } catch (error) {
+      alert(
+        `Something went wrong while fetching the users: \n${handleError(error)}`
+      );
     }
+  }
 
-    async startGame(id) {
-        try{
-            sessionStorage.setItem("isValid", "true");
-            this.setState({alreadyClicked: true});
-            const requestBody = JSON.stringify({
-                gameId: id
-            });
+  async leaveGame() {
+    try {
+      sessionStorage.setItem("isValid", "true");
+      const requestBody = JSON.stringify({
+        gameId: this.state.lobby.gameId,
+        userId: sessionStorage.UserId,
+      });
 
-            const response = await api.put(`/games/lobby`, requestBody)
-            //console.log(response.data);
+      const response = await api.put("/games/out", requestBody);
+      //console.log(response.data);
 
-            sessionStorage.setItem("GameRoundId",response.data.gameRoundId);
-            sessionStorage.setItem("points",0.0);
-            sessionStorage.setItem("TotalGameRounds", "X");
-            this.props.history.push(`/games/${id}`); 
-        }
-        catch (error) {
-            alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
-        }
+      sessionStorage.removeItem("PlayerId");
+
+      this.props.history.push(`/lobby`);
+    } catch (error) {
+      alert(`Something went wrong during the login: \n${handleError(error)}`);
     }
+  }
 
-    async leaveGame(){
-        try{
-            sessionStorage.setItem("isValid", "true");
-            const requestBody = JSON.stringify({
-                gameId: this.state.lobby.gameId,
-                userId: sessionStorage.UserId
-            });
-        
-            const response = await api.put('/games/out', requestBody);
-            //console.log(response.data);
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-            sessionStorage.removeItem("PlayerId");
+  async updateGameLobby() {
+    const pathname = this.props.location.pathname;
+    var temp = pathname.split("/");
+    var lastsegment = temp[temp.length - 1];
 
-            this.props.history.push(`/lobby`); 
-        }
-        catch (error) {
-            alert(`Something went wrong during the login: \n${handleError(error)}`);
-          }
+    const response = await api.get(`/games/${lastsegment}/lobby`);
+    //console.log(response.data);
+
+    this.setState({ lobby: response.data });
+    if (this.state.lobby.gameStatus == "RUNNING") {
+      sessionStorage.setItem("isValid", "true");
+      const response = await api.get(`/games/lobby/${this.state.lobby.gameId}`);
+      sessionStorage.setItem("GameRoundId", response.data.gameRoundId);
+      sessionStorage.setItem("points", 0.0);
+      sessionStorage.setItem("TotalGameRounds", "X");
+      this.props.history.push(`/games/${this.state.lobby.gameId}`);
     }
+  }
 
-    componentWillUnmount(){
-        clearInterval(this.interval);
+  async componentDidMount() {
+    try {
+      sessionStorage.setItem("isValid", "false");
+      const pathName = this.props.location.pathname;
+      sessionStorage.setItem("pathName", pathName);
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      this.updateGameLobby();
+      this.interval = setInterval(async () => {
+        this.updateGameLobby();
+      }, 1000);
+    } catch (error) {
+      alert(
+        `Something went wrong while fetching the users: \n${handleError(error)}`
+      );
     }
+  }
 
-    async updateGameLobby(){
-        const pathname = this.props.location.pathname;   
-        var temp = pathname.split('/');
-        var lastsegment = temp[temp.length-1];
-
-        const response = await api.get(`/games/${lastsegment}/lobby`);
-        //console.log(response.data);
-
-        this.setState({ lobby: response.data }); 
-        if(this.state.lobby.gameStatus == "RUNNING") {
-            sessionStorage.setItem("isValid", "true");
-            const response = await api.get(`/games/lobby/${this.state.lobby.gameId}`)
-            sessionStorage.setItem("GameRoundId",response.data.gameRoundId);
-            sessionStorage.setItem("points",0.0);
-            sessionStorage.setItem("TotalGameRounds", "X");
-            this.props.history.push(`/games/${this.state.lobby.gameId}`); 
-        }         
-    }
-
-    async componentDidMount() {
-        try {
-            sessionStorage.setItem("isValid", "false");
-            const pathName = this.props.location.pathname;
-            sessionStorage.setItem("pathName", pathName);
-
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            this.updateGameLobby();
-            this.interval = setInterval(async() => {
-                this.updateGameLobby();
-            },1000);
-        } catch (error) {
-            alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
-        }
-    }
-
-    render() {
-        return (
-            <BaseContainer>
-                <FormContainer>
-                    <Container>
-                        <h2>GAME LOBBY</h2>
-                        <h3>Waiting for the game to start...</h3>
-                        <p>All players of this game:</p>
-                        {!this.state.lobby ? (
-                            <Spinner />
-                        ) : (sessionStorage.getItem("UserId") == this.state.lobby.adminPlayerId?(
-                            <div>
-                                <Users>
-                                    {this.state.lobby.players.map(player => {
-                                        let admin = this.state.lobby.adminPlayerId;
-                                        return (
-                                            <PlayerContainer>
-                                                <GameLobbyEntity  player={player} admin = {admin}/>
-                                            </PlayerContainer>
-                                        );
-                                    })}
-                                </Users>
-                                <ButtonContainer>
-                                <CustomizedButton
-                                    disabled={(this.state.lobby.numberOfPlayers < 3 || this.state.lobby.numberOfPlayers > 7 || this.state.alreadyClicked)}
-                                    color1={"palegreen"} color2={"limegreen"} width = {"60%"}
-                                    onClick={() => {
-                                        this.startGame(this.state.lobby.gameId);
-                                    }}
-                                >
-                                    Start Game
-                                </CustomizedButton>
-                                </ButtonContainer>
-                               
-                            </div>
-                            ):(
-                                <div>
-                                    <Users>
-                                        {this.state.lobby.players.map(player => {
-                                            let admin = this.state.lobby.adminPlayerId;
-                                            return (
-                                                <PlayerContainer>
-                                                    <GameLobbyEntity  player={player} admin = {admin}/>
-                                                </PlayerContainer>
-                                            );
-                                        })}
-                                    </Users>
-                                    <ButtonContainer>
-                                        <CustomizedButton
-                                            color1={"red"} color2={"darkred"} width = {"60%"}
-                                            onClick={() => {
-                                                this.leaveGame();
-                                            }}
-                                        >
-                                            Leave Game
-                                        </CustomizedButton>
-                                    </ButtonContainer>
-                                </div>
-                            )
-                        )}
-                    </Container>
-                </FormContainer>
-            </BaseContainer>
-        );
-    }
+  render() {
+    return (
+      <BaseContainer>
+        <FormContainer>
+          <Container>
+            <h2>GAME LOBBY</h2>
+            <h3>Waiting for the game to start...</h3>
+            <p>All players of this game:</p>
+            {!this.state.lobby ? (
+              <Spinner />
+            ) : sessionStorage.getItem("UserId") ==
+              this.state.lobby.adminPlayerId ? (
+              <div>
+                <Users>
+                  {this.state.lobby.players.map((player) => {
+                    let admin = this.state.lobby.adminPlayerId;
+                    return (
+                      <PlayerContainer>
+                        <GameLobbyEntity player={player} admin={admin} />
+                      </PlayerContainer>
+                    );
+                  })}
+                </Users>
+                <ButtonContainer>
+                  <CustomizedButton
+                    disabled={
+                      this.state.lobby.numberOfPlayers < 3 ||
+                      this.state.lobby.numberOfPlayers > 7 ||
+                      this.state.alreadyClicked
+                    }
+                    color1={"palegreen"}
+                    color2={"limegreen"}
+                    width={"60%"}
+                    onClick={() => {
+                      this.startGame(this.state.lobby.gameId);
+                    }}
+                  >
+                    Start Game
+                  </CustomizedButton>
+                </ButtonContainer>
+              </div>
+            ) : (
+              <div>
+                <Users>
+                  {this.state.lobby.players.map((player) => {
+                    let admin = this.state.lobby.adminPlayerId;
+                    return (
+                      <PlayerContainer>
+                        <GameLobbyEntity player={player} admin={admin} />
+                      </PlayerContainer>
+                    );
+                  })}
+                </Users>
+                <ButtonContainer>
+                  <CustomizedButton
+                    color1={"red"}
+                    color2={"darkred"}
+                    width={"60%"}
+                    onClick={() => {
+                      this.leaveGame();
+                    }}
+                  >
+                    Leave Game
+                  </CustomizedButton>
+                </ButtonContainer>
+              </div>
+            )}
+          </Container>
+        </FormContainer>
+      </BaseContainer>
+    );
+  }
 }
 
 export default withRouter(GameLobby);
